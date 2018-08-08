6c11be3c59fcaf1d77b9b6c51c1c3d3c6eda95c2
Gustavo Saiani <gustavo@poe.ma>
2018-08-07 21:39:37 -0300

    Add basic teams page

--- components/team/Main/index.js
+++ components/team/Main/index.js
@@ -1,18 +1,17 @@
 import {Component, Fragment} from 'react'
 import Container from 'components/shared/Main/styles'
-import GreatMoments from '../GreatMoments'
-import Achievements from '../Achievements'
+import GreatMoments from 'components/athlete/GreatMoments'
+import Achievements from 'components/athlete/Achievements'
 
 export default class AthleteMain extends Component {
   render() {
-    const {fullName, sport, jobTitle, birthDate, birthPlace,
+    const {fullName, sport, jobTitle, activeDateFrom, activeDateTo,
            hometown, photo, achievements} = this.props
     return (
       <Container>
         <h1>{fullName}</h1>
-        <h2>{sport} · {jobTitle}</h2>
-        <h3>Born in {birthDate} in {birthPlace}</h3>
-        <h3>Lives in {hometown}</h3>
+        <h2>{sport}</h2>
+        <h3>From {activeDateFrom} in {activeDateTo}</h3>
 
         <Achievements achievements={achievements} />
         <GreatMoments photo={photo} />
