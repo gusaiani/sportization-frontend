20d1749b466287c55c28fb4efdb421f0cbae4e5b
Gustavo Saiani <gustavo@poe.ma>
2018-09-09 18:45:26 -0300

    A new home page

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import {Component} from 'react'
+import Page from 'components/shared/Page'
+
+export default class Sportization extends Component {
+  render() {
+    return (
+      <Page>
+        <h1>Home Page</h1>
+      </Page>
+    )
+  }
+}
